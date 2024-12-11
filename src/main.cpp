d397239afd4e22a8daf58224e3cfd95a8c817328
root <root@pc-thom-windows>
2024-12-11 21:56:03 +0100

    Day 8

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include "d03/D03.h"
 #include "d04/D04.h"
 #include "d05/D05.h"
-#include "d06/D06.h"
-#include "d07/D07.h"
+// #include "d06/D06.h"
+// #include "d07/D07.h"
+#include "d08/D08.h"
 
 int main()
 {
@@ -13,8 +14,9 @@ int main()
     D03().run();
     D04().run();
     D05().run();
-    D06().run(); // p2 800ms runtime, not great
-    D07().run(); // p2 8 second runtime, bad
+    // D06().run(); // p2 800ms runtime, not great
+    // D07().run(); // p2 8 second runtime, bad
+    D08().run();
     
     return 0;
 }
