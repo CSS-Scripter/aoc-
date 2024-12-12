85efad64dec4dc7b6622d65e59c49d69a56e7251
CSS-Scripter <thomleenman@gmail.com>
2024-12-12 14:49:56 +0100

    i don't know

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 // #include "d06/D06.h"
 // #include "d07/D07.h"
 #include "d08/D08.h"
+#include "d09/D09.h"
 #include "d11/D11.h"
 
 int main()
@@ -18,9 +19,8 @@ int main()
     // D06().run(); // p2 800ms runtime, not great
     // D07().run(); // p2 8 second runtime, bad
     D08().run();
-
-    // D09 & D10 
-
+    D09().run();
+    // D10().run(); // Doesn't exist yet
     D11().run();
     
     return 0;
